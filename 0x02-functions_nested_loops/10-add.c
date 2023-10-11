ff87bcc8db52d865ac3948666cf6504077a88361
Ahmed <ahmedanwarkhaliel@gmail.com>
2023-10-10 17:36:23 -0700

    101

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - add two integers
+ *
+ * @a: fisrt i/o
+ * @b: second i/o
+ *
+ * Return: sum of a and b
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
