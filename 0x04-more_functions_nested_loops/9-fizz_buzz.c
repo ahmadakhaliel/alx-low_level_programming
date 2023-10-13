8d597b3c2d3de3845e8c7e4fc091b6faaf8085f4
Ahmed <ahmedanwarkhaliel@gmail.com>
2023-10-13 14:17:58 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - print numbers 1 - 100 followed by a new line
+ *	numbers that are divided by 3 print Fizz
+ *	numbers that are divided by 5 print Buzz
+ *	numbers divided by both 3 and 5 print FizzBuzz
+ *
+ * Return: always 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && !(num % 5 == 0))
+			printf("Fizz");
+		else if (num % 5 == 0 && !(num % 3 == 0))
+			printf("Buzz");
+		else if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", num);
+
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+}
