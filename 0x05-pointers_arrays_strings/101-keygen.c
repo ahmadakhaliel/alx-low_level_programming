f48c007ac3b0d094a46f08eb0306e482253d285f
Ahmed <ahmedanwarkhaliel@gmail.com>
2023-10-16 18:52:06 -0700

    adv

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - kgen
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2775 - sum);
+	return (0);
+}
