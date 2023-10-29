28f52a61bb42e6a3593b8a74e5b406c31fd24757
Ahmed <ahmedanwarkhaliel@gmail.com>
2023-10-29 11:11:03 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the multip of 2 numb
+ * @argc: count of arguments
+ * @argv: pointer to string
+ * Return: 0 on sucess, 1 Error
+*/
+
+int main(int argc, char *argv[])
+{
+	int mult;
+
+	if (argc == 3)
+
+	{
+		mult = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mult);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
