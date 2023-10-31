d3e8b05b9b9d41e02b4ee058b5886ba0583d5c85
Ahmed <ahmedanwarkhaliel@gmail.com>
2023-10-31 11:44:37 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,50 +13,13 @@
 char *create_array(unsigned int size, char c)
 
 {
-	if (size == 0)
-	{
-		return (NULL);
-	}
+	char *n = malloc(size);
 
-	char *char_array = (char *) malloc(size * sizeof(char));
-
-	if (char_array == NULL)
-	{
-		printf("Memory allocation failed.\n");
-		return (NULL);
-	}
-
-	for (unsigned int i = 0; i < size; i++)
-	{
-		char_array[i] = c;
-	}
-
-	return (char_array);
-}
-
-/**
- * main - main function
- * Return: -
-*/
-
-int main(void)
-
-{
-	unsigned int size = 5;
-	char c = 'H';
-
-	char *my_array = create_array(size, c);
-
-	if (my_array != NULL)
-	{
-		for (unsigned int i = 0; i < size; i++)
-		{
-			printf("%c ", my_array[i]);
-		}
-		printf("\n");
-
-		free(my_array);
-	}
-
+	if (size == 0 || n == 0)
 	return (0);
+
+	while (size--)
+	n[size] = c;
+
+	return (n);
 }
