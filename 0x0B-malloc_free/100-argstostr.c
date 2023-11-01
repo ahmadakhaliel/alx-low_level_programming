22b1e15af9a0fbb72cf067d4e99d10fe30c17188
Ahmed <ahmedanwarkhaliel@gmail.com>
2023-10-31 17:48:53 -0700

    100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,12 +1,41 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(int ac, char *av[])
+ * *argstostr - concatenates *arguments of a program
+ * @ac: int
+ * @av: pointer
+ * Return: result
+*/
+
+char *argstostr(int ac, char **av) 
 {
-    char *s;
+    if (ac == 0 || av == NULL) 
+    {
+        return (NULL);
+    }
+
+    int total_length = 0;
+    
+    for (int i = 0; i < ac; i++) 
+    {
+        total_length += strlen(av[i]) + 1; // +1 for the newline character
+    }
+
+    char *result = malloc(sizeof(char) * total_length);
+    if (result == NULL) 
+    {
+        return (NULL);
+    }
+
+    int current_index = 0;
+    
+    for (int i = 0; i < ac; i++) 
+    {
+        strcpy(result + current_index, av[i]);
+        current_index += strlen(av[i]);
+        result[current_index] = '\n';
+        current_index++;
+    }
+}
