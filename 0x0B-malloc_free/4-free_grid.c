c95e8fe85062af8e22f144bb770521df61307928
Ahmed <ahmedanwarkhaliel@gmail.com>
2023-10-31 17:39:15 -0700

    4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,12 +3,17 @@
 #include <stdlib.h>
 
 /**
- * print_grid - prints a grid of integers
- * @grid: the address of the two dimensional grid
- * @width: width of the grid
- * @height: height of the grid
- *
- * Return: Nothing.
- */
-void print_grid(int **grid, int width, int height)
+ * free_grid - function that frees a 2 dimensional grid
+ * @grid: grid of memories
+ * @height: int
+ * Return: void
+*/
+
+void free_grid(int **grid, int height)
 {
+	int i = 0;
+
+	for (; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
