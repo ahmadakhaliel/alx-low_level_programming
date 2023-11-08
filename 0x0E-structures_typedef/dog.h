7d5b423fb08ada2163f044fbfa27309a47f5a3b2
Ahmed <ahmedanwarkhaliel@gmail.com>
2023-11-07 17:22:14 -0800

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - str. dog
+ * @name: character data type
+ * @age: character folat type
+ * @owner: character data type
+ * Description: firts structure created by me
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
