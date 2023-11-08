cb54193882336d784f82237f4ea4ef7695816808
Ahmed <ahmedanwarkhaliel@gmail.com>
2023-11-07 18:37:24 -0800

    5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,18 @@
-#include <sstdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
- * free dog - frees the fns
+ * free_dog - frees the fns
  * @d: dogs
  * Return: void.
 */
 void free_dog(dog_t *d)
 {
-	if(d)
+	if (d)
 	{
 		if (d->name)
 			free(d->name);
-		else (d->owner)
+		if (d->owner)
 			free(d->owner);
 		free(d);
 	}
