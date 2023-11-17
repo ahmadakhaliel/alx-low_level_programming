3ff9c2b05f48c20127ddeee21cb81d210a51aa3c
Ahmed <ahmedanwarkhaliel@gmail.com>
2023-11-16 17:20:37 -0800

    0

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,29 +1,49 @@
 #include "lists.h"
 
-/**
- * add_node - adds a node to the list start 
- * @head: address of pointer to head node
- * @str: str field
- * Return: size of list
-*/
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str) 
 {
-	list_t *new_head = malloc(sizeof(list_t));
+    if (str == NULL)
+        return (NULL);
 
-	if (!head || !new_head)
-		return (NULL);
-	if (str)
-	{
-		new_head->str = strdup(str);
-		if (!new_head->str)
-		{
-			free(new_head);
-			return (NULL);
-		}
-		new_head->len = _strlen(new_head->str);
-	}
+    list_t *new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	new_head->next = *head;
-	*head = new_head;
-	return (new_head);
+    new_node->str = strdup(str);
+
+    if (new_node->str == NULL) {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->len = strlen(str);
+    new_node->next = *head;
+    *head = new_node;
+
+    return (new_node);
 }
+
+int main() 
+{
+    list_t *head = NULL;
+
+    add_node(&head, "World");
+    add_node(&head, "Hello");
+
+    list_t *current = head;
+    while (current != NULL) {
+        printf("[%u] %s\n", current->len, current->str);
+        current = current->next;
+    }
+
+
+    while (head != NULL) {
+        list_t *temp = head;
+        head = head->next;
+        free(temp->str);
+        free(temp);
+    }
+
+    return (0);
+}
+
